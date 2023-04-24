7a91912d2475fa09e7a0771dac5a80f5d40d2774
jwilcox5 <123770450+jwilcox5@users.noreply.github.com>
2023-04-24 15:18:40 -0400

    Add files via upload

---
+++ src/application/treeutil/NavbarElements.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+  
+export const Nav = styled.nav`
+  background: #0f0974;
+  height: 85px;
+  display: flex;
+  padding: 0.2rem calc((100vw - 1000px) / 2);
+`;
+  
+export const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+`;
